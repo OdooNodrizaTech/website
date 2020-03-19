00719dd34b731fa565ef6125914e001ae859a820
Informatica Arelux <informatica@arelux.com>
2020-03-19 07:50:26 +0100

    init

---
+++ ont_website_noindex/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Ont Website Noindex',
+    'version': "10.0.1.0.0,    
+    'author': 'Odoo Nodriza Tech (ONT)',
+    'website': 'https://nodrizatech.com/',
+    'category': 'Tools',
+    'license': 'AGPL-3',
+    'depends': ['website'],
+    'data': [
+        'views/website-config-settings.xml',
+        'templates/assets.xml',
+        'templates/website_layout.xml',
+        'templates/website_navbar.xml',
+    ],
+    'installable': True,
+    'auto_install': False,    
+}
